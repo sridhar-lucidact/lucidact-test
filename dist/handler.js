79550fd959edb4a18d25075672dea6141769a192
Sridhar Gudimela <sridhar@lucidact.com>
2023-11-30 12:00:43 -0800

    changed function name to lowecase

--- dist/handler.js
+++ dist/handler.js
@@ -1,5 +1,14 @@
 'use strict';
-export const generateRandomNumber = async (event) => {
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+export const generateRandomNumber = (event) => __awaiter(void 0, void 0, void 0, function* () {
     const randomNumber = Math.random() * 100;
     console.log('randomNumber', randomNumber);
     return {
@@ -8,5 +17,5 @@ export const generateRandomNumber = async (event) => {
             randomNumber
         }
     };
-};
+});
 //# sourceMappingURL=handler.js.map
