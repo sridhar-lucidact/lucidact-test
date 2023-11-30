041a451c7204d1cec00b9045a1e31f41c6f1584c
Sridhar Gudimela <sridhar@lucidact.com>
2023-11-30 09:18:39 -0800

    added conf and sample files for typescript

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,7 @@
 'use strict';
-
-module.exports.generateRandomNumber = async (event) => {
+export async function generateRandomNumber(event) {
   const randomNumber = parseInt(Math.random()*100);
   console.log('randomNumber',randomNumber);
+  console.log(new moment());
   return randomNumber;
-};
+}
