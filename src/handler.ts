89c69eadda5cf21d4bbe66a6572c12fefb8c2584
Sridhar Gudimela <sridhar@lucidact.com>
2023-12-01 07:08:32 -0800

    changed ping to function

--- src/handler.ts
+++ src/handler.ts
@@ -5,7 +5,7 @@ const {ping} = lucid;
 export const generateRandomNumber: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const randomNumber = Math.random()*100;
   console.log('randomNumber',randomNumber);  
-  console.log('ping', ping());
+  ping();
   const body = JSON.stringify({randomNumber});
   return {
     statusCode: 200,
