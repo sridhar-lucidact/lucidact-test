d91a6940da3c111a79454e7434150a40d93ddca7
Sridhar Gudimela <sridhar@lucidact.com>
2023-12-01 07:47:58 -0800

    changed lucid import

--- src/handler.ts
+++ src/handler.ts
@@ -1,13 +1,14 @@
 'use strict';
 import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
-import {ping} from 'lucid';
+import lucid from 'lucid';
 //const {ping} = lucid;
 export const generateRandomNumber: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const randomNumber = Math.random()*100;
   console.log('randomNumber',randomNumber);  
-  console.log(typeof ping);  
-  console.log(ping);
-  ping();
+  console.log('lucid',lucid);
+  console.log(typeof lucid.ping);  
+  console.log(lucid.ping);
+  lucid.ping();
   const body = JSON.stringify({randomNumber});
   return {
     statusCode: 200,
