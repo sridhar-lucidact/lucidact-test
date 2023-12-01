a302e61b7aa6bcd7355c5a2fe5a70306408421a4
Sridhar Gudimela <sridhar@lucidact.com>
2023-12-01 07:17:02 -0800

    added extra console logs

--- src/handler.ts
+++ src/handler.ts
@@ -5,6 +5,8 @@ const {ping} = lucid;
 export const generateRandomNumber: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const randomNumber = Math.random()*100;
   console.log('randomNumber',randomNumber);  
+  console.log(typeof ping);  
+  console.log(ping);
   ping();
   const body = JSON.stringify({randomNumber});
   return {
