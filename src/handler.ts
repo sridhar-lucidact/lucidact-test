e1dcfc64b3cff3a9abcfec61a6f570a1e56fa4e2
Sridhar Gudimela <sridhar@lucidact.com>
2024-01-23 18:47:07 -0800

    Revert "commented lucid-health and ping method"
    This reverts commit f0e4c78fd341cbcbd12183c6667d49a50c699172.

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,11 @@
 'use strict';
 import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} from 'aws-lambda';
-//import * as lucid from 'lucid-health';
-//const {ping} = lucid;
+import * as lucid from 'lucid-health';
+const {ping} = lucid;
 export const generateRandomNumber: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const randomNumber = Math.random()*100;
   console.log('randomNumber',randomNumber);  
-  //ping();
+  ping();
   const body = JSON.stringify({randomNumber});
   return {
     statusCode: 200,
