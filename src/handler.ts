a90b4038b751c7120a73428fd46b655a3f4bba5b
Sridhar Gudimela <sridhar@lucidact.com>
2024-02-07 17:29:14 -0800

    added process.env.NODE_PATH

--- src/handler.ts
+++ src/handler.ts
@@ -4,9 +4,9 @@ import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult} fro
 //const {ping} = lucid;
 export const generateRandomNumber: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const randomNumber = Math.random()*100;
-  console.log('randomNumber:',randomNumber);  
+  console.log('randomNumber:',randomNumber, process.env.NODE_PATH);  
   //ping();
-  const body = JSON.stringify({randomNumber});
+  const body = JSON.stringify({randomNumber, path: process.env.NODE_PATH});
   return {
     statusCode: 200,
     body
